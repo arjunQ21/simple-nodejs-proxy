ad18fa3c1d2ae2fd15e493a80195d77b5f5f10a4
arjunq21 <arjunadhikari789@gmail.com>
2023-06-14 19:21:04 +0545

    Changed origin mode:

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+require("dotenv").config(".env");
 httpProxy = require("http-proxy");
 console.log(process.env.PORT);
 
 try {
   console.log("Starting proxy server on "+process.env.PROXY_URL+" on port " + process.env.PORT);
-  httpProxy.createProxyServer({ target: process.env.PROXY_URL }).listen(8000);
+  httpProxy.createProxyServer({ target: process.env.PROXY_URL, changeOrigin: true  }).listen(process.env.PORT);
   console.log("Started proxy server on port " + process.env.PORT);
 } catch (e) {
   console.log("Error starting proxy server on port " + process.env.PORT);
